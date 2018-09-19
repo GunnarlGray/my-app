769798ef04e83db24b5ac61bb0c31da7d8e10802
Gunnar Gray <gunnargray@Gunnars-MBP.domain>
2018-09-19 07:38:29 -0500

    Animations & Transitions

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
     <div className="Hero">
       <div className="HeroGroup">
         <h1>Gunnar Gray</h1>
-        <p>Digital Product Designer</p>
+        <h2>Digital Product Designer</h2>
         <p>I design and prototype mobile apps, websites, and digital experiences. Currently, I work at Livefront where I’ve had the privilege to design large-scale products for clients such as Target, United Healthcare, Delta Dental, General Mills, Medtronic, Geico & many more.</p>
         <Link to="/page-2/">Get In Touch</Link>
       </div>
