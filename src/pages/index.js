5aefcac4e17b1b085696ec4a191bab0ded6b075f
Gunnar Gray <gunnargray@Gunnars-MBP.domain>
2018-09-18 19:36:46 -0500

    Contact Button

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <div>
+      <h1>Gunnar Gray</h1>
+      <p>Digital Product Designer</p>
+      <p>I design and prototype mobile apps, websites, and digital experiences. Currently, I work at Livefront where I’ve had the privilege to design large-scale products for clients such as Target, United Healthcare, Delta Dental, General Mills, Medtronic, Geico & many more.</p>
+      <Link to="/page-2/">Get In Touch</Link>
+    </div>
+  </Layout>
+)
+
+export default IndexPage
