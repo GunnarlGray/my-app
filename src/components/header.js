95ef0dd893ca91668a76114cb316187e995ce5ba
Gunnar Gray <gunnargray@Gunnars-MBP.domain>
2018-09-19 19:30:08 -0500

    States & Events

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,43 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import './layout.css'
 
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
 
-const Header = ({ siteTitle }) => (
-<div className="Header">
-  <div className="HeaderGroup">
-    <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
-    <Link to="/Hello">Hello</Link>
-    <Link to="/About">About</Link>
-    <Link to="/Lab">Lab</Link>
-    <Link to="/Contact">Contact</Link>
-  
-  </div>
-  
-</div>
-)
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll',
+    this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true})
+    } else {
+      this.setState({ hasScrolled: false})
+    }
+  }
+  render() {
+    return(
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
+          <Link to="/Hello">Hello</Link>
+          <Link to="/About">About</Link>
+          <Link to="/Lab">Lab</Link>
+          <Link to="/Contact">Contact</Link>
+        </div>
+      </div>  
+    )
+  }
+}
 
 export default Header
